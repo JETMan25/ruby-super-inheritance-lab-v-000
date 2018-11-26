3c01479aa8fab5ad7ea7f1b8875765c05a0daf2b
jontenthoff <jontenthoff@hotmail.com>
2018-11-26 17:32:18 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,7 @@
 class ChattyStudent < Student
   def hello
     super
+<<<<<<< HEAD
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
@@ -16,4 +17,9 @@ class ChattyStudent < Student
     puts "Pick me!"
     puts "Pick me!"
   end
+=======
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Deaf last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay, well let me just tell you who died..."
+  end 
+
+>>>>>>> 401332a8760c241655cf52c39b8fb9198854759e
 end
